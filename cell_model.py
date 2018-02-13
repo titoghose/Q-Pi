a567b6ffd4422a1aaed90310f3bff86685cef334
upamanyughose <titoghose@gmail.com>
2018-02-14 00:35:07 +0530

    Added to code to generate contours per cross section

---
+++ cell_model.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+
+
+def genCellStructure(x, )
+for i in range(1, 60, 1):
+    for
--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,6 @@ from scipy.spatial import ConvexHull
 
 ix, iy, fx, fy = 0, 0, 0, 0
 drawing = False
-img = None
 
 
 def handle_mouse(event, x, y, flags, params):
@@ -37,8 +36,14 @@ while True:
 final_contours = []
 final_contours = np.array(final_contours)
 
-for ind, i in enumerate(os.listdir("./membrane_cell/c2/")):
+try:
+    os.mkdir("./contours" + str(ix) + "_" + str(iy) + "_" + str(fx) + "_" + str(fy) + "/")
+except OSError:
+    None
 
+for ind, i in enumerate(os.listdir("./membrane_cell/c2/")):
+    if i.startswith("."):
+        continue
     img = cv2.imread(os.getcwd() + "/membrane_cell/c2/" + i)
     img = cv2.resize(img, (480, 480))
     img = img[iy:fy, ix:fx, 0]
@@ -59,47 +64,64 @@ for ind, i in enumerate(os.listdir("./membrane_cell/c2/")):
 
     im = img.copy()
 
-    im2, contours, hierarchy = cv2.findContours(im, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_TC89_L1)
+    _, contours, hierarchy = cv2.findContours(im, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_TC89_L1)
 
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-    new_contours = []
-    for j, c in enumerate(contours):
-        area = cv2.contourArea(c)
-        if area > 50:
-            new_contours.append(c)
-    img = cv2.drawContours(img, new_contours, -1, (0, 255, 0), 1)
+    img_temp = np.zeros(img.shape)
 
-    try:
-        cont = np.array(new_contours[0])
-        cont = (cont * 2048 * 0.0695) / 480.
-        cont = np.insert(cont, 2, (ind+1) * 0.2, axis=2)
-        cont = np.squeeze(cont)
-        if final_contours.shape[0] == 0:
-            final_contours = cont
-        else:
-            final_contours = np.vstack((final_contours, cont))
-    except IndexError:
-        c = 0
+    new_contours = contours
+    # new_contours = []
+    # for j, c in enumerate(contours):
+    #     area = cv2.contourArea(c)
+    #     if area > 50:
+    #         new_contours.append(c)
 
-    #cv2.imshow("Original", im_og)
-    #cv2.imshow("Processed", img)
-    #c = cv2.waitKey(0)
+    if len(new_contours) != 0:
+        new_contours = np.squeeze(np.array(new_contours))
+        ellipse = cv2.fitEllipse(new_contours)
+        img = cv2.ellipse(img_temp, ellipse, (255, 255, 255), -1)
 
-    #print ind, " : ", thresh_val, "(", chr(c), ")"
+    _, contours, hierarchy = cv2.findContours(img, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_TC89_L1)
 
-#final_contours = np.array(final_contours)
+    # img = cv2.drawContours(img, new_contours, -1, (0, 255, 0), 1)
 
-print final_contours.shape
+    # try:
+    #     cont = np.array(new_contours[0])
+    #     cont = np.squeeze(cont)
+    #
+    #     fig = plt.figure()
+    #     ch = ConvexHull(cont)
+    #     plt.ylim(0, 60)
+    #     plt.xlim(0, 60)
+    #     for s in ch.simplices:
+    #         plt.plot(cont[s, 0], cont[s, 1], 'k-')
+    #     fig.savefig("./contours" + str(ix) + "_" + str(iy) + "_" + str(fx) + "_" + str(fy) + "/" + i)
+    #     print "./contours" + str(ix) + "_" + str(iy) + "_" + str(fx) + "_" + str(fy) + "/" + i
+    #     plt.close(fig)
+    #
+    #     cont = (cont * 2048 * 0.0695) / 480.
+    #     cont = np.insert(cont, 2, (ind + 1) * 0.2, axis=1)
+    #
+    #     if final_contours.shape[0] == 0:
+    #         final_contours = cont
+    #     else:
+    #         final_contours = np.vstack((final_contours, cont))
+    # except IndexError:
+    #     c = 0
 
-ch = ConvexHull(final_contours)
+    cv2.imshow("Original", im_og)
+    cv2.imshow("Processed", img)
+    c = cv2.waitKey(0)
 
-'''
-fig = plt.figure()
-for s in ch.simplices:
-    plt.plot(final_contours[s, 0], final_contours[s, 1], final_contours[s, 2], 'r.')
 
-plt.show()
-'''
-
-print ch.volume
+# print final_contours.shape
+#
+# ch = ConvexHull(final_contours)
+#
+# fig = plt.figure()
+# for s in ch.simplices:
+#     plt.plot(final_contours[s, 0], final_contours[s, 1], final_contours[s, 2], 'r.')
+# plt.show()
+#
+# print ch.volume
