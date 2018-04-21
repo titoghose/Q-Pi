3e661253c2b0c76922ca8cb8c82caeb49f6de44c
Upamanyu Ghose <titoghose@gmail.com>
2018-04-22 03:12:03 +0530

    Changed opencv version in setup.py

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo easy_install pip
 sudo pip install numpy
 sudo pip install scipy
 sudo pip install matplotlib
-sudo pip install opencv-python
+sudo pip install opencv-python==3.3.0.10
 sudo pip install pims_nd2
 sudo pip install vtk
 sudo pip install mayavi
